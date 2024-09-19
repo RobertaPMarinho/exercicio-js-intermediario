491490fc9d5bfc22bb4c2615d24ee00720e38e7d
Roberta Pereira Marinho <robertapmarinho@gmail.com>
2024-09-19 11:40:36 -0300

    first commit - exercicio01

---
+++ exercicio01/index.js
@@ -0,0 +1,9 @@
+function trocarDeCor() {
+
+  let mudancaDeCor = document.getElementsByClassName('primeiroQuadrado')
+
+  let primeiroQuadrado = mudancaDeCor[0]
+
+  primeiroQuadrado.style.backgroundColor = "yellow"
+}
+
